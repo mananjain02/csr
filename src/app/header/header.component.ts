cee15ec8e004805dcc88776639b864fdf06cae4c
Manan Jain <mananjainoffi@gmail.com>
2023-07-17 14:05:18 +0530

    configured the site for small devices

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
 
@@ -8,12 +8,23 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit, OnDestroy {
+  screenWidth: number;
+  @ViewChild('toggleButton', { static: false }) toggleButton: ElementRef;
+
+  // function to get current width of screen
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.screenWidth = window.innerWidth;
+  }
+
   role: String;
   // remove initialization once auth is ready
   authenticated: Boolean = true;
   authenticateSubscription: Subscription;
   roleSubscription: Subscription;
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {
+    this.screenWidth = window.innerWidth;
+  }
 
   ngOnInit(): void {
     this.authenticateSubscription = this.authService.getAuthStatusObservable()
@@ -31,7 +42,26 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.authenticateSubscription.unsubscribe();
   }
 
+  // function to automatically close
+  // header on small devices
+  autoToggle() {
+    let scrollToTop = window.setInterval(() => {
+        let pos = window.pageYOffset;
+        if (pos > 0) {
+            window.scrollTo(0, pos - 125);
+        } else {
+            window.clearInterval(scrollToTop);
+        }
+    }, 16);
+
+    if(this.screenWidth > 991) return;
+    setTimeout(() => {
+      this.toggleButton.nativeElement.click();
+    }, 75)
+  }
+
   onLogout() {
     this.authService.logout()
+    this.autoToggle();
   }
 }
